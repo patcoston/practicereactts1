da07f10bd218538f21d776c3428edf11601ef580
Patrick Coston <patcoston@gmail.com>
2022-12-11 15:15:41 -0500

    Displayed dots where clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { MouseEvent } from 'react'
+import { MouseEvent, useState } from 'react'
 import './App.css'
 
+interface Dots {
+  x: number
+  y: number
+  visible: boolean
+}
+
+const dotWidth = 10
+const dotHeight = 10
+
 function App() {
+  const [dots, setDots] = useState<Dots[]>([])
   const undo = () => {}
   const redo = () => {}
   const makeDot = (e: MouseEvent) => {
-    console.log(e)
+    const x = e.clientX - dotWidth / 2
+    const y = e.clientY - dotHeight / 2
+    setDots([...dots, { x, y, visible: true }])
   }
 
   return (
@@ -14,7 +26,15 @@ function App() {
         <button onClick={undo}>Undo</button>
         <button onClick={redo}>Redo</button>
       </div>
-      <div className="dots" onClick={makeDot}></div>
+      <div className="dots" onClick={makeDot}>
+        {dots.map((dot, i) => (
+          <div
+            key={`dot-${i}`}
+            className="dot"
+            style={{ left: dot.x, top: dot.y }}
+          />
+        ))}
+      </div>
     </div>
   )
 }

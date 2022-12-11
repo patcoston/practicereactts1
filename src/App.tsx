ca7cabbfd7f20d0aa0a831bbc2c644ca7988bee8
Patrick Coston <patcoston@gmail.com>
2022-12-11 15:03:31 -0500

    Added styles and event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { MouseEvent } from 'react'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  const undo = () => {}
+  const redo = () => {}
+  const makeDot = (e: MouseEvent) => {
+    console.log(e)
+  }
+
+  return (
+    <div className="App">
+      <div className="header">
+        <button onClick={undo}>Undo</button>
+        <button onClick={redo}>Redo</button>
+      </div>
+      <div className="dots" onClick={makeDot}></div>
+    </div>
+  )
 }
 
 export default App
